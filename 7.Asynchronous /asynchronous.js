13c9d9717ab289390bf7c15857a71b07ccf4c793
marufbellah <mdmaruf5511507@gmail.com>
2023-12-16 00:21:18 +0600

    first commit

---
+++ 7.Asynchronous /asynchronous.js
@@ -0,0 +1,13 @@
+// Synchronous============================
+
+const processOrder = () => {
+  console.log(`processing order for customer 1`);
+
+  var currentTime = new Date().getTime();
+  while (currentTime + 3000 >= new Date().getTime());
+  console.log(`order processed for customer 1`);
+};
+
+console.log(`take order for customer 1`);
+processOrder();
+console.log(`completed order for customer 1`);
