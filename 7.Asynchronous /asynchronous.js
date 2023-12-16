d38c77c5ab63855d4923fe82bb17dd3c64c35055
marufbellah <mdmaruf5511507@gmail.com>
2023-12-16 16:37:34 +0600

    callback function is done

--- 7.Asynchronous /asynchronous.js
+++ 7.Asynchronous /asynchronous.js
@@ -1,13 +1,56 @@
 // Synchronous============================
 
-const processOrder = () => {
-  console.log(`processing order for customer 1`);
+// const processOrder = () => {
+//   console.log(`processing order for customer 1`);
 
-  var currentTime = new Date().getTime();
-  while (currentTime + 3000 >= new Date().getTime());
-  console.log(`order processed for customer 1`);
+//   var currentTime = new Date().getTime();
+//   while (currentTime + 3000 >= new Date().getTime());
+//   console.log(`order processed for customer 1`);
+// };
+
+// console.log(`take order for customer 1`);
+// processOrder();
+// console.log(`completed order for customer 1`);
+
+// Asynchronous============================
+
+// const processOrder1 = () => {
+//   console.log(`processing order for customer 1`);
+
+//   setTimeout(() => {
+//     console.log(`order processed for customer 1`);
+//   }, 3000);
+// };
+
+// console.log(`take order for customer 1`);
+// processOrder1();
+// console.log(`completed order for customer 1`);
+
+
+// CallBack function ==============================
+
+const takeOrder = (customer, CallBack) => {
+  console.log(`take order for ${customer}`);
+  CallBack(customer)
 };
 
-console.log(`take order for customer 1`);
-processOrder();
-console.log(`completed order for customer 1`);
+const processOrder = (customer, CallBack) => {
+  console.log(`processing order for ${customer}`);
+
+  setTimeout(() => {
+    console.log(`cooking completed`);
+    console.log(`order processed for ${customer}`);
+    CallBack(customer)
+  }, 3000);
+};
+
+const completeOrder = (customer) => {
+  console.log(`completed order for ${customer}`);
+};
+
+takeOrder('maruf bellah', (customer) => {
+  processOrder(customer, (customer) => {
+    completeOrder(customer)
+  })
+  
+})
